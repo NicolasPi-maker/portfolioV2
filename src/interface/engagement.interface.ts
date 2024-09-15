20aa76c834f539240e93f58fba0cbd54f8e18a5e
Pinier-Nicolas <pinier74nicolas@gmail.com>
2024-09-15 16:32:59 +0200

    add engagement and footer section

---
+++ src/interface/engagement.interface.ts
@@ -0,0 +1,9 @@
+export interface EngagementInterface {
+  title: string;
+  description: string;
+  imagePath: string;
+  imageAlt: string;
+  reversed: boolean;
+  auraColor: string;
+  customClass: string;
+}
